136b3a257368e4dcc0fe763f6ed946b4f75230fe
John Villas <joaovictor.vbt@gmail.com>
2022-03-13 23:16:04 -0300

    feat: user entity model

---
+++ api/src/entities/User.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryColumn,
+} from "typeorm"
+import { v4 as uuid } from "uuid"
+
+@Entity("users")
+export class User {
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  phone: number
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
