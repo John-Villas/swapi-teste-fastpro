999a8caeb1d77b2b60aa0fb88bc7425482230bcf
John Villas <joaovictor.vbt@gmail.com>
2022-03-13 23:16:34 -0300

    feat: create user service and controller

---
+++ api/src/controllers/CreateUserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express"
+import { CreateUserService } from "../services/CreateUserService"
+
+export class CreateUserController {
+  async handle(request: Request, response: Response) {
+    const { name, phone, email, password } = request.body
+
+    const service = new CreateUserService()
+    const result = await service.execute({ name, phone, email, password })
+
+    if (result instanceof Error) {
+      return response.status(400).json(result.message)
+    }
+
+    return response.json(result)
+  }
+}
---
+++ api/src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { getRepository } from "typeorm"
+import { User } from "../entities/User"
+
+interface UserRequest {
+  name: string
+  phone: number
+  email: string
+  password: string
+}
+
+export class CreateUserService {
+  async execute({
+    name,
+    phone,
+    email,
+    password,
+  }: UserRequest): Promise<User | Error> {
+    const repo = getRepository(User)
+
+    if (await repo.findOne({ email })) {
+      return new Error("This email address is already in use")
+    }
+
+    const user = repo.create({
+      name,
+      phone,
+      email,
+      password,
+    })
+
+    await repo.save(user)
+
+    return user
+  }
+}
