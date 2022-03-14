ef668d4d83bd27b2b9306f46de2ee375eafcdc74
John Villas <joaovictor.vbt@gmail.com>
2022-03-13 23:15:37 -0300

    feat: create user migration

---
+++ api/src/database/migrations/1647121353124-CreateUser.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateUser1647121353124 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+            generationStrategy: "uuid",
+          },
+          { name: "name", type: "varchar" },
+          { name: "phone", type: "varchar" },
+          { name: "email", type: "varchar", isUnique: true },
+          { name: "password", type: "varchar" },
+          { name: "created_at", type: "timestamp", default: "now()" },
+          { name: "updated_at", type: "timestamp", default: "now()" },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users")
+  }
+}
