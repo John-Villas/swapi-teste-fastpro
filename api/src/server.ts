cb33369d1d5cfd32542db3d09797dcff48d4e8b0
John Villas <joaovictor.vbt@gmail.com>
2022-03-13 23:20:42 -0300

    update: server imports

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,14 @@
+import "reflect-metadata"
 import express from "express"
+import { routes } from "./routes"
+import "./database"
 
 const port = 3003
 const app = express()
+
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`listening on port ${port}...`)
