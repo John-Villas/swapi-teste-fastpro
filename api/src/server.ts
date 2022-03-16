985007bb32a9580c715ae432c6b3e9b4d6843634
John Villas <joaovictor.vbt@gmail.com>
2022-03-15 21:21:40 -0300

    feat: cors implementation

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,12 +2,13 @@ import "reflect-metadata"
 import express from "express"
 import { routes } from "./routes"
 import "./database"
+import cors from "cors"
 
 const port = 3003
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use(routes)
 
 app.listen(port, () => {
